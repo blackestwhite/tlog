d5c91c786c7d799f51fe419bbf8c65819a4dd1a3
blackestwhite <pesaregoal@gmail.com>
2022-02-12 09:39:15 +0330

    return response

--- tg.go
+++ tg.go
@@ -1,6 +1,7 @@
 package tlog
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,7 +15,12 @@ type Bot struct {
 	admin string
 }
 
-func (b *Bot) SendMessage(msg string) {
+type Response struct {
+	Ok     bool        `json:"ok"`
+	Result interface{} `json:"result"`
+}
+
+func (b *Bot) SendMessage(msg string) Response {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=<pre>%s</pre>&parse_mode=html", b.token, b.admin, msg)
 	// http request to url
 	cli := &http.Client{}
@@ -26,6 +32,12 @@ func (b *Bot) SendMessage(msg string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	wg.Done()
 	defer resp.Body.Close()
+	defer wg.Done()
+	res := Response{}
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
 }

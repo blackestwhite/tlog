2bd075580e26bb1320e8ae289cbe13a682f759eb
blackestwhite <pesaregoal@gmail.com>
2022-10-21 21:26:29 +0330

    fix test

--- tg_test.go
+++ tg_test.go
@@ -1,11 +1,15 @@
 package tlog
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestSendMessage(t *testing.T) {
 	b := &Bot{
-		token: "5103837914:AAFkNHrB53QWuxl9HX7WJm8H97uOT83RbKQ",
-		admin: "1422816851",
+		token:  "5103837914:AAFkNHrB53QWuxl9HX7WJm8H97uOT83RbKQ",
+		admin:  "1422816851",
+		client: &http.Client{},
 	}
 	res := b.SendMessage("test")
 	if !res.Ok {
